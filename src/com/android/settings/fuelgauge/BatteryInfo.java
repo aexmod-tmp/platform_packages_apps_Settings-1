5d3499a033773bb97f8fc5a6c3adff6024d077ff
Anushek Prasal <anushekprasal@gmail.com>
2019-12-15 03:02:41 +0530

    Settings: Display 'Warp charging' for warp charge in battery settings [2/2]
    Change-Id: Id6e813dd61500bb6445bbf89bf6f27866fade27b
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/settings/fuelgauge/BatteryInfo.java
+++ src/com/android/settings/fuelgauge/BatteryInfo.java
@@ -285,6 +285,8 @@ public class BatteryInfo {
                 BatteryManager.BATTERY_STATUS_UNKNOWN);
         final boolean dashChargeStatus = batteryBroadcast.getBooleanExtra(
                 BatteryManager.EXTRA_DASH_CHARGER, false);
+        final boolean warpChargeStatus = batteryBroadcast.getBooleanExtra(
+                BatteryManager.EXTRA_WARP_CHARGER, false);
         info.discharging = false;
         info.suggestionLabel = null;
         int dockDefenderMode = BatteryUtils.getCurrentDockDefenderMode(context, info);
@@ -308,6 +310,9 @@ public class BatteryInfo {
             if (dashChargeStatus)
                 info.remainingLabel = chargeTimeMs <= 0 ? null : context.getString(
                         R.string.power_remaining_dash_charging_duration_only, timeString);
+            else if (warpChargeStatus)
+                info.remainingLabel = chargeTimeMs <= 0 ? null : context.getString(
+                        R.string.power_remaining_warp_charging_duration_only, timeString);
             else
                 info.remainingLabel = chargeTimeMs <= 0 ? null : context.getString(
                         R.string.power_remaining_charging_duration_only, timeString);
