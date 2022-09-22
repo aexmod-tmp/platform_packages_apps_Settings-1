LineageOS/android_packages_apps_LineageParts

LiveDisplaySettings: Fix outdoor mode preference on hwc2
Change-Id: I7ecbc68cb7bc2eb70544f8f5bc3e3d9d2943e4d7

src/org/lineageos/lineageparts/livedisplay/LiveDisplaySettings.java
@@ -310,6 +310,9 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements S
     }
 
     private void updateModeSummary() {
+        if (ColorDisplayController.isAvailable(getContext())) {
+            return; // Do nothing if device has hwc2 support
+        }
         int mode = mLiveDisplayManager.getMode();
 
         int index = ArrayUtils.indexOf(mModeValues, String.valueOf(mode));

aexmod-tmp/platform_packages_apps_Settings-1

Signed-off-by: DennySPB <dennyspb@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/android/settings/livedisplay/LiveDisplaySettings.java
@@ -316,6 +316,10 @@ public class LiveDisplaySettings extends SettingsPreferenceFragment implements
     }
 
     private void updateModeSummary() {
+        if (ColorDisplayManager.isNightDisplayAvailable(getContext())) {
+            return; // Do nothing if device has hwc2 support
+        }
+
         int mode = mLiveDisplayManager.getMode();
 
         int index = ArrayUtils.indexOf(mModeValues, String.valueOf(mode));