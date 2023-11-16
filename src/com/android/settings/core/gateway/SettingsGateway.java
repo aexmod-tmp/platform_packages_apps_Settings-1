syberia-project/platform_packages_apps_Settings

Add SyberiaSettings dashboard
Change-Id: I8543b8af6f508e2ead192c7ee1fdc9603cc5b06d
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.syberia.settings.SyberiaSettings;
+
 public class SettingsGateway {
 
     /**
@@ -283,7 +285,8 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
-            GlobalActionsPanelSettings.class.getName()
+            GlobalActionsPanelSettings.class.getName(),
+            SyberiaSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -322,5 +325,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.SyberiaSettingsActivity.class.getName(),
     };
-}
+            }

aexmod-tmp/platform_packages_apps_Settings-1

Signed-off-by: DennySPb <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -192,6 +192,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.syberia.settings.SyberiaSettings;
+
 public class SettingsGateway {
 
     /**
@@ -371,7 +373,8 @@ public class SettingsGateway {
             NfcAndPaymentFragment.class.getName(),
             ColorAndMotionFragment.class.getName(),
             LongBackgroundTasksDetails.class.getName(),
-            RegionalPreferencesEntriesFragment.class.getName()
+            RegionalPreferencesEntriesFragment.class.getName(),
+            SyberiaSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -418,5 +421,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.SyberiaSettingsActivity.class.getName(),
     };
-}
+            }